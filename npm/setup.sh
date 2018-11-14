a8ab6034b297b3d42f6000423c836cc83acd7b2c
Wei Xia <xiawei910@gmail.com>
2018-11-14 23:34:05 +0800

    alias cnpm

--- npm/setup.sh
+++ npm/setup.sh
@@ -1,5 +1,8 @@
-npm config set registry http://r.cnpmjs.org/
+echo '\n#alias for cnpm\nalias cnpm="npm --registry=https://registry.npm.taobao.org \
+      --cache=$HOME/.npm/.cache/cnpm \
+      --disturl=https://npm.taobao.org/dist \
+      --userconfig=$HOME/.cnpmrc"' >> ~/.bashrc && source ~/.bashrc
 
-npm i -g npm to update
+# npm i -g npm to update
 
-npm install phantomjs
+cnpm install phantomjs
