5bfa68667ef77f7e7ae65966915a255dc84ee4ad
Wei Xia <xiawei910@gmail.com>
2022-05-30 22:23:09 +0800

    add conda

---
+++ conda/setup.sh
@@ -0,0 +1,10 @@
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+sh Miniconda3-latest-Linux-x86_64.sh
+
+conda update conda
+
+conda create -n chaozaiji python=3.6
+# helper command
+# conda activate chaozaiji
+# conda deactivate
--- setup.sh
+++ setup.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-cd ~
-mv ~/.vimrc ~/.vimrc.bak
-rm -rf ~/.vim
-rm -rf ~/.vim_tmp
-ln -s ~/.dotfiles/vimrc .vimrc
-ln -s ~/.dotfiles/vim_runtime .vim_runtime
-mkdir -p ~/.vim_tmp/undodir
-mkdir -p ~/.vim_tmp/backup
-mv ~/.bash_profile ~/.bash_profile.bak
-ln -s ~/.dotfiles/bash_profile .bash_profile
-mv ~/.tmux.conf .tmux.conf.bak
-ln -s ~/.dotfiles/tmux.conf .tmux.conf
+# Please do not use this script, install software independly
 
-bash ./setup.sh
+
+#cd ~
+#mv ~/.vimrc ~/.vimrc.bak
+#rm -rf ~/.vim
+#rm -rf ~/.vim_tmp
+#ln -s ~/.dotfiles/vim/vimrc .vimrc
+#ln -s ~/.dotfiles/vim/vim_runtime .vim_runtime
+#mkdir -p ~/.vim_tmp/undodir
+#mkdir -p ~/.vim_tmp/backup
+#mv ~/.bash_profile ~/.bash_profile.bak
+#ln -s ~/.dotfiles/bash_profile .bash_profile
+#mv ~/.tmux.conf .tmux.conf.bak
+#ln -s ~/.dotfiles/tmux.conf .tmux.conf
+
+#bash ./setup.sh
+
 
