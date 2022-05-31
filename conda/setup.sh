bbe52b4a1b5aa997866fb9a3eea82a527ca0e0d7
Wei Xia <xiawei910@gmail.com>
2022-05-31 16:22:12 +0800

    change chaozaiji to python3.8

--- conda/setup.sh
+++ conda/setup.sh
@@ -4,7 +4,7 @@ sh Miniconda3-latest-Linux-x86_64.sh
 
 conda update conda
 
-conda create -n chaozaiji python=3.6
+conda create -n chaozaiji python=3.8
 # helper command
 # conda activate chaozaiji
 # conda deactivate
