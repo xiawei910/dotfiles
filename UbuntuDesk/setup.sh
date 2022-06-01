7e0e0fc1531d313af65a7500134598985b696643
Wei Xia <xiawei910@gmail.com>
2022-06-01 15:37:42 +0800

    add sougou input

---
+++ UbuntuDesk/setup.sh
@@ -0,0 +1,8 @@
+
+# Sougou pingyin
+# Support:
+# https://pinyin.sogou.com/linux/guide
+sudo apt-get install fcitx #Sougou pingyin
+sudo cp /usr/share/applications/fcitx.desktop /etc/xdg/autostart/ #AutoStart
+sudo apt install libqt5qml5 libqt5quick5 libqt5quickwidgets5 qml-module-qtquick2
+sudo apt install libgsettings-qt1
