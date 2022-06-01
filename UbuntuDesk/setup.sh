9180c507be93577f12d344eef768cfa60a4be0f6
Wei Xia <xiawei910@gmail.com>
2022-06-01 21:11:54 +0800

    add latex in Ubuntu

--- UbuntuDesk/setup.sh
+++ UbuntuDesk/setup.sh
@@ -6,3 +6,6 @@ sudo apt-get install fcitx #Sougou pingyin
 sudo cp /usr/share/applications/fcitx.desktop /etc/xdg/autostart/ #AutoStart
 sudo apt install libqt5qml5 libqt5quick5 libqt5quickwidgets5 qml-module-qtquick2
 sudo apt install libgsettings-qt1
+
+# Latex
+sudo apt-get install texlive-latex-base latex-cjk-all texlive-latex-extra texmaker texlive-xetex texlive-publishers latexmk
